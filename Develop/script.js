e6c0390cb7752678170b142b64222d7bbdd278ab
Tyric Jones <tyrictjones@gmail.com>
2022-08-09 21:21:14 -0700

    initial prompts and character sets

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,39 @@
 // Assignment code here
+var passwordLength = window.prompt("Enter a password length.");
+if (passwordLength > 128 || passwordLength < 8 || !passwordLength) {
+  window.alert("Password must be between 8 and 128 characters. Try again!");
+  //call function again
+};
+console.log("Password length: " + passwordLength);
+var includeLowercase = window.confirm("Include lowercase letters?");
+console.log("lowercase " + includeLowercase);
+var includeUppercase = window.confirm("Include uppercase letters?");
+console.log("uppercase " + includeUppercase);
+var includeNumbers = window.confirm("Include numbers?");
+console.log("numbers " + includeNumbers);
+var includeSpecial = window.confirm("Include special characters?");
+console.log("special " + includeSpecial);
+
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "[", "]", "\\", "^", "_", "`", "{", "}", "~"];
+
+var fullSet = [];
+if (includeLowercase) {
+  fullSet = fullSet.concat(lowercase);
+};
+if (includeUppercase) {
+  fullSet = fullSet.concat(uppercase);
+};
+if (includeNumbers) {
+  fullSet = fullSet.concat(numbers);
+};
+if (includeSpecial) {
+  fullSet = fullSet.concat(special);
+};
+console.log(fullSet);
+
 
 
 // Get references to the #generate element
