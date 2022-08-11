8b26d8d5570a3c2eba5ee0dd516a37b2a0c38b9f
Tyric Jones <tyrictjones@gmail.com>
2022-08-10 18:09:33 -0700

    more js comments and user must select 1 char type

--- Develop/script.js
+++ Develop/script.js
@@ -9,35 +9,42 @@ var generatePassword = function () {
   //If user does not enter a password length, prompt user to try again
   if (!passwordLength) {
     while(!passwordLength) {
-      window.alert("You must enter a length for your password. Try again!");
+      window.alert("You must enter a length for your password.");
       passwordLength = window.prompt("Enter a password length.");
     }
   }
 
   //If length is not 8-128 characters or left blank, prompt user to try again
-  if (passwordLength > 128 || passwordLength < 8) {
-    while(passwordLength > 128 || passwordLength < 8) {
-      window.alert("Your password must be between 8 and 128 characters. Try again!");
+  if (passwordLength < 8 || passwordLength > 128) {
+    while(passwordLength < 8 || passwordLength > 128) {
+      window.alert("Your password must be between 8 and 128 characters.");
       passwordLength = window.prompt("Enter a password length.");
     }
   }
 
-  console.log("Password length: " + passwordLength);
-
-  var includeLowercase = window.confirm("Include lowercase letters?");
-  console.log("lowercase " + includeLowercase);
-  var includeUppercase = window.confirm("Include uppercase letters?");
-  console.log("uppercase " + includeUppercase);
-  var includeNumbers = window.confirm("Include numbers?");
-  console.log("numbers " + includeNumbers);
-  var includeSpecial = window.confirm("Include special characters?");
-  console.log("special " + includeSpecial);
-
+  //Prompt user to choose which characters to include
+  //Use while-loop to repeat until at least one of the variables is true (user selects at least one type)
+  var includeLowercase = false;
+  var includeUppercase = false;
+  var includeNumbers = false;
+  var includeSpecial = false;
+  while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    var includeLowercase = window.confirm("Include lowercase letters?");
+    var includeUppercase = window.confirm("Include uppercase letters?");
+    var includeNumbers = window.confirm("Include numbers?");
+    var includeSpecial = window.confirm("Include special characters?");
+    if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+      window.alert("You must select at least 1 character type.");
+    }
+  }
+  
+  //Base set of arrays to combine based on which character types the user selects
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   var special = ["!", "\"", "#", "$", "%", "\&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "[", "]", "\\", "^", "_", "`", "{", "}", "~"];
 
+  //Use boolean values of user selections to combine arrays into a full set
   var fullSet = [];
   if (includeLowercase) {
     fullSet = fullSet.concat(lowercase);
@@ -51,8 +58,9 @@ var generatePassword = function () {
   if (includeSpecial) {
     fullSet = fullSet.concat(special);
   };
-  console.log(fullSet);
 
+  //Start with a blank string and use math.random() to add a character from the full set to the end of the string
+  //Cycle through the loop based on the password length the user chose
   newPassword = "";
 
   for (i = 0; i <= passwordLength; i++) {
